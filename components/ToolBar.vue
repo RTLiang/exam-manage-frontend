d520c96bfbbd00614d6e9f30941ed5a45f8bb16b
RTLiang <54389220+RTLiang@users.noreply.github.com>
2024-05-16 12:34:46 +0800

    move toolbar to outside of pages, making it a component

---
+++ components/ToolBar.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="banner">
+    <h1>省考系统</h1>
+    <p>省着点考</p>
+</div>
+
+<div class="tool-bar">
+    <div class="button-container">
+        <b>常驻功能区</b>
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger">Danger</el-button>
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger">Danger</el-button>
+    </div>
+</div>
+
+</template>
+<style scoped>
+.banner {
+    background-color: #333;
+    color: #fff;
+    padding: 5px;
+    text-align: center;
+}
+
+
+.tool-bar {
+    background-color: #f0f0f0;
+
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.button-container {
+    max-width: 80%;
+    /* Take up 70% of the screen width */
+    margin: 0 auto;
+    /* Center horizontally */
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,15 @@
 <template>
-    <div class="banner">
-        <h1>省考系统</h1>
-        <p>省着点考</p>
-    </div>
-
-    <div class="tool-bar">
-        <div class="button-container">
-            <el-button>Default</el-button>
-            <el-button type="primary">Primary</el-button>
-            <el-button type="success">Success</el-button>
-            <el-button type="info">Info</el-button>
-            <el-button type="warning">Warning</el-button>
-            <el-button type="danger">Danger</el-button>
-            <el-button>Default</el-button>
-            <el-button type="primary">Primary</el-button>
-            <el-button type="success">Success</el-button>
-            <el-button type="info">Info</el-button>
-            <el-button type="warning">Warning</el-button>
-            <el-button type="danger">Danger</el-button>
-        </div>
-    </div>
-
+    <ToolBar />
+    <NuxtPage />
+    aaa
 </template>
 
 
-<style scoped>
-.banner {
-    background-color: #333;
-    color: #fff;
-    padding: 5px;
-    text-align: center;
+
+<script>
+export default {
+    components: {
+        ToolBar: () => import('~/components/ToolBar.vue')
+    }
 }
-
-
-.tool-bar {
-    background-color: #f0f0f0;
-
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.button-container {
-    max-width: 80%;
-    /* Take up 70% of the screen width */
-    margin: 0 auto;
-    /* Center horizontally */
-}
-</style>
+</script>
