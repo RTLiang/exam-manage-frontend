9107746f6f4de8e493bccdbc4d3f06d8bd777602
RTLiang <54389220+RTLiang@users.noreply.github.com>
2024-05-20 16:39:55 +0800

    机构账号

--- pages/admin/individual_account.vue
+++ pages/admin/individual_account.vue
@@ -13,6 +13,8 @@
                 <el-table-column prop="email" label="电子邮箱" required />
                 <el-table-column prop="Id_number" label="身份证号" required />
                 <el-table-column prop="institution" label="所属机构" />
+                <el-table-column prop="whitelisted" label="是否是白名单" sortable :formatter="ShiFouType"
+                    :filter-method="filterIsWhitelisted" />
                 <el-table-column label="操作" width="100">
                     <template v-slot="scope">
                         <el-button @click="handleEdit(scope.row)" type="primary" size="small">修改</el-button>
@@ -38,6 +40,7 @@ export default {
                     phone_number: '13812345678',
                     email: 'liming@example.com',
                     Id_number: '420101199001011234',
+                    whitelisted: false,
                 },
                 {
                     id: 2,
@@ -45,7 +48,8 @@ export default {
                     phone_number: '13698765432',
                     email: 'wangxiaohong@example.com',
                     Id_number: '420102199002021234',
-                    institution: '清华大学'
+                    institution: '清华大学',
+                    whitelisted: false,
                 },
                 {
                     id: 3,
@@ -53,7 +57,8 @@ export default {
                     phone_number: '15888888888',
                     email: 'zhaowei@example.com',
                     Id_number: '420103199003031234',
-                    institution: '中国人民大学'
+                    institution: '中国人民大学',
+                    whitelisted: false,
                 },
                 {
                     id: 4,
@@ -61,7 +66,8 @@ export default {
                     phone_number: '15111111111',
                     email: 'lili@example.com',
                     Id_number: '420104199004041234',
-                    institution: '北京交通大学'
+                    institution: '北京交通大学',
+                    whitelisted: true,
                 },
                 {
                     id: 5,
@@ -69,6 +75,7 @@ export default {
                     phone_number: '13999999999',
                     email: 'zhangwei@example.com',
                     Id_number: '420105199005051234',
+                    whitelisted: true,
 
                 },
                 {
@@ -77,6 +84,7 @@ export default {
                     phone_number: '13888889999',
                     email: 'wuqiong@example.com',
                     Id_number: '420106199006061234',
+                    whitelisted: true,
 
                 }
             ]
@@ -106,7 +114,19 @@ export default {
     methods: {
         handleEdit(row) {
             console.log(`Edit button clicked for row ${row.name}`)
-        }
+        }, 
+        filterIsWhitelisted(value, row) {
+            return row.IsWhitelisted === value;
+        },
+        ShiFouType(row, column,cellValue) {
+            if (row, column, cellValue) {
+                return ("是");
+            }
+            else {
+                return ("否");
+            }
+        },
+
     }
 }
 </script>
--- pages/admin/institution_account.vue
+++ pages/admin/institution_account.vue
@@ -1,1 +1,93 @@
-<template></template>
+<template>
+    <Banner />
+    <Admin />
+    <br>
+    <div class="institution_list_box">
+        <br>
+        <h2 style="text-align: center;">机构账号</h2>
+        <div class="institution_list">
+            <el-input v-model="searchQuery" placeholder="搜索" />
+            <el-table :data="filteredInstitutions" border style="width: 100%">
+                <el-table-column fixed prop="id" label="账号" required />
+                <el-table-column prop="name" label="机构名称" required />
+                <el-table-column label="操作" width="100">
+                    <template v-slot="scope">
+                        <el-button @click="handleEdit(scope.row)" type="primary" size="small">修改</el-button>
+                    </template>
+                </el-table-column>
+
+
+            </el-table>
+        </div>
+        <br>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchQuery: '',
+            institutions: [
+                {
+                    id: 1,
+                    name: '北京大学',
+                },
+                {
+                    id: 2,
+                    name: '清华大学',
+                },
+                {
+                    id: 3,
+                    name: '中国人民大学',
+                },
+                {
+                    id: 4,
+                    name: '北京交通大学',
+                },
+                {
+                    id: 5,
+                    name: '北京航空航天大学',
+                },
+                {
+                    id: 6,
+                    name: '北京理工大学',
+                }
+            ]
+
+        }
+    },
+    computed: {
+        filteredInstitutions() {
+            const searchQuery = this.searchQuery.toLowerCase();
+            return this.institutions.filter(institution => {
+                const nameMatch = institution.name.toLowerCase().includes(searchQuery);
+                return (
+                    nameMatch 
+                );
+            });
+        }
+    },
+
+
+    methods: {
+        handleEdit(row) {
+            console.log(`Edit button clicked for row ${row.name}`)
+        }
+    }
+}
+</script>
+<style>
+.institution_list {
+    width: 95%;
+    margin: 0 auto;
+
+}
+
+.institution_list_box {
+    background-color: #f3f3f3;
+    border-radius: 5px;
+    width: 90%;
+    margin: 0 auto;
+}
+</style>
