b2b44e6c27a8af6358f93da22b8909c0ce1a098d
RTLiang <54389220+RTLiang@users.noreply.github.com>
2024-05-23 15:04:36 +0800

    feat: Update user type property in Welcome.vue and select_exam.vue
    The code changes update the property name for the user type from `usr_type` to `user_type`. This change ensures consistency in the naming convention and improves code readability.

--- components/Checkout.vue
+++ components/Checkout.vue
@@ -1,134 +1,205 @@
 <template>
-    <Banner />
-    <br>
-    <div class="exam_info_page">
-        <h1 v-if="type === 'individual'" style="text-align: center;">考生缴费</h1>
-        <h1 v-if="type === 'edu'" style="text-align: center;">机构缴费</h1>
+  <Banner />
+  <br>
+  <div class="exam_info_page">
+    <h1 v-if="type === 'individual'" style="text-align: center;">考生缴费</h1>
+    <h1 v-if="type === 'edu'" style="text-align: center;">机构缴费</h1>
+    <el-row :gutter="20">
+      <el-col :span="6">
 
-        <el-row :gutter="20">
-            <el-col :span="6">
-                <div v-if="type === 'individual'">
-                    <ExamInfo type="individual" />
-                </div>
-                <div v-if="type === 'edu'">
-                    <ExamInfo type="edu" :studentNumbers=studentNumbers />
-                </div>
-            </el-col>
-            <el-col :span="18">
-                <div class="exam_pay">
-                    <h2 style="text-align: center;">考试费用： {{ exam.fee * studentNumbers }} 人民币</h2>
-                    <h3 style="text-align: center;">请选择支付方式</h3>
-                    <el-divider />
-                    <el-image src="https://t.alipayobjects.com/images/T11rdgXbFkXXXXXXXX.png"
-                        class="payment_methods_image" @click="handlePaymentMethodClick('支付宝')" alt="支付宝" />
-                    <el-image src="https://wx.gtimg.com/pay_en/img/common/logo.svg" class="payment_methods_image"
-                        @click="handlePaymentMethodClick('微信支付')" alt="微信支付" />
-                    <el-image src="https://cdn.visa.com/v2/assets/images/logos/visa/blue/logo.png"
-                        class="payment_methods_image" @click="handlePaymentMethodClick('VISA')" alt="VISA" />
-                </div>
+        <div class="exam_info">
+          <h2>考试信息</h2>
+          <p v-if="type === 'individual'"><b>考生姓名：</b>{{ student.name }}</p>
+          <p v-if="type === 'edu'"><b>考试人数：</b>{{ studentNumbers }}</p>
+          <p><b>考试科目：</b>{{ exam.examName }}</p>
+          <p><b>考试时间：</b>{{ formatTime(exam.startExamTime, exam.endExamTime) }}</p>
+          <p><b>考试地点：</b>{{ exam.examcenterlocation }}</p>
+        </div>
 
-            </el-col>
-        </el-row>
 
-    </div>
+      </el-col>
+      <el-col :span="18">
+        <div class="exam_pay">
+          <h2 style="text-align: center;">考试费用： {{ exam.examPayment * studentNumbers }} 人民币</h2>
+          <h3 style="text-align: center;">请选择支付方式</h3>
+          <el-divider />
+          <el-image src="https://t.alipayobjects.com/images/T11rdgXbFkXXXXXXXX.png" class="payment_methods_image"
+            @click="handlePaymentMethodClick('支付宝')" alt="支付宝" />
+          <el-image src="https://wx.gtimg.com/pay_en/img/common/logo.svg" class="payment_methods_image"
+            @click="handlePaymentMethodClick('微信支付')" alt="微信支付" />
+          <el-image src="https://cdn.visa.com/v2/assets/images/logos/visa/blue/logo.png" class="payment_methods_image"
+            @click="handlePaymentMethodClick('VISA')" alt="VISA" />
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, inject } from 'vue';
 import { ElMessage } from 'element-plus';
+import api from '../axios';
+
+import { useRouter } from 'vue-router';
+
+import Banner from '@/components/Banner.vue';
+import ExamInfo from '@/components/ExamInfo.vue';
 
 export default {
-
-    props: {
-        type: {
-            type: String,
-            required: true
-        },
-        studentNumbers: {
-            type: Number,
-            default: 1,
-        }
+  components: {
+    Banner,
+    ExamInfo
+  },
+  props: {
+    type: {
+      type: String,
+      required: true
     },
+    studentNumbers: {
+      type: Number,
+      default: 1,
+    }
+  },
+  data() {
+    return {
+      student: {
+        name: '',
+        idNumber: '',
+        phone: '',
+      },
+      exam: {
+        examCenterId: '',
+        centerName: '',
+        cityName: '',
+        districtName: '',
+        examId: '',
+        examName: '',
+        startExamTime: '',
+        endExamTime: '',
+        examPayment: -1,
+        examcenterlocation: '',
+      },
+      userId: '',
+    };
+  },
+  setup() {
+    const router = useRouter();
 
-    data() {
-        return {
-            student: {
-                name: '张三',
-                id: '123456789012345678',
-                phone: '13800138000'
-            },
-            exam: {
-                subject: '科目三',
-                examTime: '2024年5月1日 15:00-17:00',
-                deadline: '2024年4月30日 23:00',
-                fee: 648,
-                location: '北京市海淀区中关村大街1号',
-            },
-        };
+    const handlePaymentMethodClick = (paymentMethod: string) => {
+      ElMessage({
+        message: `缴费成功！`,
+        type: 'success',
+      });
+      setTimeout(() => {
+        window.location.href = `./additional_procedure?examId=${this.exam.examId}&userId=${this.userId}`;
+      }, 1000); // wait for 2 seconds before redirecting and reloading
+    };
+
+    return {
+      handlePaymentMethodClick,
+    };
+  },
+  methods: {
+    async fetchexaminfo() {
+      console.log("fetchexaminfo");
+      const examId = this.$route.query.examId;
+      const userId = this.$route.query.userId;
+      const centerId = this.$route.query.centerId;
+
+      try {
+        const res = await api.post(`/apply/exam?examId=${examId}&userId=${userId}&centerId=${centerId}`);
+        console.log(res);
+        const data = res.data;
+        this.exam.examCenterId = data.examCenterId;
+        this.exam.centerName = data.centerName;
+        this.exam.cityName = data.cityName;
+        this.exam.districtName = data.districtName;
+        this.exam.examId = data.examId;
+        this.exam.examName = data.examName;
+        this.exam.startExamTime = data.startExamTime;
+        this.exam.endExamTime = data.endExamTime;
+        this.exam.examPayment = data.examPayment;
+        this.exam.examcenterlocation = data.centerLocation;
+        this.userId = data.userId;
+        console.log("this.exam");
+        console.log(this.exam);
+      } catch (error) {
+        console.log(error);
+      };
+
+      try {
+        const res = await api.get(`/examinee/info?userId=${userId}`);
+        const examinee = res.data.examinee;
+        this.student.name = examinee.name;
+        this.student.idNumber = examinee.IDNumber;
+        this.student.phone = examinee.phone;
+        console.log("============");
+        console.log(res);
+        console.log(this.student);
+      } catch (error) {
+        console.log(error);
+      };
+      console.log('Student Name:', this.student.name);
+      console.log('Exam Name:', this.exam.examName);
+      console.log('Start Time:', this.exam.startExamTime);
+      console.log('End Time:', this.exam.endExamTime);
+      console.log('Location:', this.exam.examcenterlocation);
+
     },
+    formatTime(startTime: string, endTime: string) {
+      const start = new Date(startTime);
+      const end = new Date(endTime);
 
-    setup() {
-        const router = useRouter();
+      const year = start.getFullYear();
+      const month = start.getMonth() + 1; // Months are 0-based in JavaScript
+      const date = start.getDate();
+      const startHour = start.getHours();
+      const startMinute = start.getMinutes();
+      const startSecond = start.getSeconds();
 
-        const handlePaymentMethodClick = (paymentMethod: string) => {
-            ElMessage({
-                message: `缴费成功！`,
-                type: 'success',
-            });
+      const endHour = end.getHours();
+      const endMinute = end.getMinutes();
+      const endSecond = end.getSeconds();
 
-            setTimeout(() => {
-                window.location.href = './additional_procedure';
-            }, 1000); // wait for 2 seconds before redirecting and reloading
-        };
-
-
-        return {
-            handlePaymentMethodClick,
-        };
-    },
-
+      return `${year}年${month}月${date}日${startHour}:${startMinute}:${startSecond}到${endHour}:${endMinute}:${endSecond}`;
+    }
+  },
+  created() {
+    console.log(this.$route.query);
+    this.fetchexaminfo();
+  }
 };
 </script>
 
-
 <style scoped>
 .exam_info_page {
-    padding: 20px;
-    margin: auto 7%;
-    background-color: rgb(221, 221, 221);
-    border-radius: 5px;
-    /* text-color */
+  padding: 20px;
+  margin: auto 7%;
+  background-color: rgb(221, 221, 221);
+  border-radius: 5px;
 }
-
 
 .payment_methods_image {
-    width: 200px;
-    /* set the width to a fixed value */
-    height: 60px;
-    /* set the height to a fixed value */
-    margin: 0 20px;
-    cursor: pointer;
-    background-color: #eaeaea;
-    /* add a white background color */
-    border-radius: 10px;
-    /* add a rounded corner effect */
-    border: none;
-    /* remove the border */
-    padding: 6px 10px;
-    /* add 16px padding top/bottom and 20px padding left/right */
-    display: inline-block;
-    /* make the element inline-block to contain the padding */
-
+  width: 200px;
+  height: 60px;
+  margin: 0 20px;
+  cursor: pointer;
+  background-color: #eaeaea;
+  border-radius: 10px;
+  border: none;
+  padding: 6px 10px;
+  display: inline-block;
 }
 
-
-
 .exam_pay {
+  background-color: #a6d1e8;
+  padding: 20px;
+  border-radius: 5px;
+  margin-bottom: 20px;
+}
 
-    background-color: #a6d1e8;
-    padding: 20px;
-    border-radius: 5px;
-    margin-bottom: 20px;
-
+.exam_info {
+  background-color: #d7bdbd;
+  border-radius: 5px;
+  padding: 20px;
 }
 </style>
